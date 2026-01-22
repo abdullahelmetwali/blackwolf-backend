6db222809d280c0e4722353ea554a1cfe023ae1c
abdullahelmetwali <abdullahelmetwali@icloud.com>
2026-01-22 12:05:17 +0200

    fixs to vercel

--- src/app.ts
+++ src/app.ts
@@ -24,7 +24,7 @@ app.use(cookieParser());
 // CORS
 app.use(
     cors({
-        origin: ["http://localhost:3000", "domain"],
+        origin: ["http://localhost:3000", "https://express-learn-wine.vercel.app/"],
         methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
         allowedHeaders: ["Content-Type", "Authorization"],
     })
